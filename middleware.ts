760765dde5fd2455b2cdd345455ec287c26a3b8b
Rowland Kirishima <rkirishima@gmail.com>
2026-04-29 22:22:42 +0900

    Exclude sitemap.xml and robots.txt from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -37,5 +37,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|demo|google.*\\.html|images/).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|demo|google.*\\.html|images/|sitemap\\.xml|robots\\.txt).*)'],
 }
